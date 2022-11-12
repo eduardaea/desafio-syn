f54c707455c79f203a9e74d5a496ee2bda4c1f25
Eduarda <duda.eduarda.ec@gmail.com>
2022-11-12 10:45:32 -0300

    Implementação de pagina completa

---
+++ myscript.js
@@ -0,0 +1,69 @@
+let validName = false;
+let validEmail = false;
+
+  function increment(){
+    var atual = document.getElementById("total").value;
+    var novo = atual - (-1); 
+    document.getElementById("total").value = novo;
+    
+  }
+  
+  function decrement(){
+    var atual = document.getElementById("total").value;
+    if(atual > 0) { 
+      var novo = atual - 1;
+      document.getElementById("total").value = novo;
+    }
+  }
+
+ 
+  $(document).ready(function(){
+    $('.input-name').on('input', function(){
+      validateSubmitButton()
+    });
+
+    $('.input-email').on('input', function(){
+       validateSubmitButton()
+    });
+
+    function validateSubmitButton(){
+      $('#send').prop('disabled', !($("#myform").valid()));
+    }
+  
+
+    
+    $("#send").click(function(event){
+      event.preventDefault();
+      $(this).addClass("hidden")
+      $('.success-msg').removeClass("hidden")
+    })
+    
+    
+    $("#myform").validate({
+      errorElement: 'span',
+      rules: {
+        
+        name: {
+          required:true,
+          minlength: 10
+        },
+        email: {
+          minlength: 5,
+          required: true,
+          email: true
+        }
+      },
+      
+      messages:{
+        name:{
+              minlength:"Por favor, informe seu nome completo.",
+              required:"Por favor, informe seu nome completo."
+        },
+        email:{
+               required:"Por favor, informe seu melhor e-mail."
+        }    
+      }
+    });
+  });
+
+  
